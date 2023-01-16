7a2088e2dd91d466e5aec6c16728da92fd8cc208
GalinaBespiata <116351519+GalinaBespiata@users.noreply.github.com>
2023-01-16 16:57:30 +0200

    form2

--- script/fetchCountries.js
+++ script/fetchCountries.js
@@ -1,12 +1,11 @@
-import { Notify } from "notiflix";
+import { Notify } from 'notiflix';
 
 export function fetchCountries(name) {
   return fetch(
     `https://restcountries.com/v3.1/name/${name}?fields=name,capital,flags,population,languages`
   ).then(response => {
-    console.log(response);
     if (!response.ok) {
-        Notify.failure("Oops, there is no country with that name");
+      Notify.failure('Oops, there is no country with that name');
     } else {
       return response.json();
     }
